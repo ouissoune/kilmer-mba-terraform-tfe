24ecaf76bf1327b5f8c13eeb1c97a3ac1c56d1c6
ouissoune ilyas <ilyasyassine41@gmail.com>
2025-12-03 15:45:18 +0100

    feat: added workspaces (#1)
    Co-authored-by: kilmer-i <ilyas.ouissoune@usmba.ac.ma>

--- locals.tf
+++ locals.tf
@@ -19,5 +19,66 @@ locals {
       project_id     = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
+
+    "fem-eci-azure-network" = {
+      description = "Automation for Azure network resources"
+      execution_mode = "remote"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-azure-network"
+      variables = [
+        {
+          key          = "location"
+          value        = "spaincentral"
+          category     = "terraform"
+          hcl          = false
+          sensitive    = false
+        },
+        {
+          key          = "rg_name"
+          value        = "myResourceGroup"
+          category     = "terraform"
+          hcl          = false
+          sensitive    = false
+        },
+        {
+          key          = "vnet_name"
+          value        = "vnet-myResourceGroup"
+          category     = "terraform"
+          hcl          = false
+          sensitive    = false
+        },
+        {
+          key          = "vnet_cidr"
+          value        = "155.55.0.0/16"
+          category     = "terraform"
+          hcl          = false
+          sensitive    = false
+        },
+        {
+          key          = "subscription_id"
+          value        = "10453ec5-bcb7-4ed7-9f6c-4e741f3611a8"
+          category     = "terraform"
+          sensitive    = true
+        },
+        {
+          key          = "subnets"
+          value        = <<EOT
+            {
+              "subnet-aks" = {
+                new_bits = 8
+                nsg = {
+                  name  = "nsg-aks"
+                  rules = []
+                }
+              }
+            }
+            EOT
+          category     = "terraform"
+          hcl          = true
+          sensitive    = false
+        }
+      ]
+
+    }
   }
 }
